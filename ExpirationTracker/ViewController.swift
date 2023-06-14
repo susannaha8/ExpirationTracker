e720048556cab5d3d9f2f5d1df04ed05031d9ad4
Jonathan <js5587@columbia.edu>
2023-06-13 22:23:29 -0400

    Initial Commit

---
+++ ExpirationTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExpirationTracker
+//
+//  Created by Jonathan and Susannah 6/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        let name = "Jonathan"
+        let date = "06/13/2023"
+                
+        let foodObject = foodObject(name: name, date: date)
+                
+        print(foodObject.name, " is expiriring today, ", foodObject.date, ". This is cool!")
+        
+    }
+    
+
+}
+
---
+++ ExpirationTracker/foodObject.swift
@@ -0,0 +1,21 @@
+//
+//  foodObject.swift
+//  ExpirationTracker
+//
+//  Created by Jonathan Socoy on 6/13/23.
+//
+
+import Foundation
+
+class foodObject {
+    
+    var name: String
+    var date: String
+    
+    init (name: String, date: String){
+        self.name = name
+        self.date = date
+    }
+    
+}
+
